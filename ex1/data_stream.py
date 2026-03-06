f48492bc24661dee83423c999cd03aac37b71106
spacotto <silvia.pacotto@gmail.com>
2026-03-06 16:56:44 +0100

    Update data_stream.py

--- ex1/data_stream.py
+++ ex1/data_stream.py
@@ -106,7 +106,7 @@ class TransactionStream(DataStream):
     def process_batch(self, data_batch: List[Any]) -> str:
         """Process a batch of data."""
         pass
-
+            
     def filter_data(self, data_batch: List[Any],
                     criteria: Optional[str] = None) -> List[Any]:
         """Filter data based on criteria."""
